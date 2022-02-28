d656dc0cd73cd1480bf6b0d8b0d7e5190651f23e
Jorge Diaz <jorgeddc@gmail.com>
2022-02-28 09:03:11 +0100

    primera

--- javaScript.js
+++ javaScript.js
@@ -1,11 +1,18 @@
 document.addEventListener("DOMContentLoaded", ()=>{
-
   
-const hamburguer = document.querySelector(".hamburguer")
+  
+window.addEventListener('scroll', fixNav);
+const home = document.querySelector(".home");
+const hamburguer = document.querySelector(".hamburguer");
 const picture = document.querySelectorAll('.picture');
-const gallery = document.querySelector('.gallery')
+const gallery = document.querySelector('.gallery');
 const dianaImage = document.querySelector('.transform');
 const close = document.querySelector('.close');
+const arrowApear = document.querySelector(".arrow-up");
+const body = document.getElementById('body');
+const  containerResponsive = document.querySelector('.container-responsive-menu')
+
+
 
 const centerImage = (diana) =>{
 console.log(diana);
@@ -14,20 +21,53 @@ diana.classList.toggle('up');}
 hamburguer.addEventListener("click", () =>{
               
         console.log('jp;')
-        hamburguer.querySelectorAll("span")[0].classList.toggle("active")
-        hamburguer.querySelectorAll("span")[1].classList.toggle("active")
-        hamburguer.querySelectorAll("span")[2].classList.toggle("active")
+        hamburguer.querySelectorAll("span")[0].classList.toggle("active");
+        hamburguer.querySelectorAll("span")[1].classList.toggle("active");
+        hamburguer.querySelectorAll("span")[2].classList.toggle("active");
+        
+        containerResponsive.classList.toggle('active');
+       
               
            
           })
           
 picture.forEach(element=>{
+  
+  
+  
   element.addEventListener(
-        'click', (e)=>{
+    'click', (e)=>{
+          if(window.scrollX < body.offsetWidth - 785)// no engranda la foto si es tambaño movil
+          
+          { 
            
+          console.log(body.offsetWidth)
           const diana = (e.target);
-        centerImage(diana);
+          centerImage(diana);
+       }
 })
+
+
     
         })
+
+
+
+        function fixNav()
+         {
+            if(window.scrollY > home.offsetHeight + 100) {
+                arrowApear.classList.add('active')
+               
+              
+            
+                } 
+            
+                else {
+                arrowApear.classList.remove('active')
+              
+              
+        
+                
+         }
+        }
       })
